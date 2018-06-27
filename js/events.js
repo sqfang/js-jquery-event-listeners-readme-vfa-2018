6b88ed633a6f9f3fdc5abd5cfe248b2d3f950541
Sophia Fang <sophiaqfang@gmail.com>
2018-06-27 23:29:11 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -12,7 +12,7 @@ function frameIt(){
 }
 
 function pressIt(){
-  $('#typing').on('keydown', function(){
+  $('#typing').on('keydown', function(key){
     if (key.which === '71'){
       alert("You've pressed G.");
     }
