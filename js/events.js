5740650a4c8bfb4fb0fb6c0f237013dfd43a85a2
Sophia Fang <sophiaqfang@gmail.com>
2018-06-27 23:44:04 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,39 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!");
+  });
+}
+
+/*function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass("tasty");
+  });
+}
+*/
+
+function frameIt(){
+  $('img').addClass('tasty');
+  return;
+}
+
+function pressIt(){
+  $('form').on('keydown', function(key){
+    if (key.which === 71){
+      alert("You've pressed G.");
+    }
+ });
+}
+
+function submitIt(){
+  $('form').on('submit', function(){
+    alert("Your form is going to be submitted now.");
+  });
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt(); //correct
+  frameIt();
+  pressIt();
+  submitIt(); //correct
 });
