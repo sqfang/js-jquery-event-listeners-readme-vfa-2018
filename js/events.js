0ec483303f3f7372e2bc18636a214c3c92b5b997
Sophia Fang <sophiaqfang@gmail.com>
2018-06-27 22:57:35 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,25 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert("Hey!")
+  });
+}
+
+function frameIt(){
+  
+}
+
+function pressIt(){
+  
+}
+
+function submitIt(){
+  
+}
 
 $(document).ready(function(){
 
 // call functions here
+
 
 });
