51ef27a2df2495a2be6139d49aeee1cfda46ac90
Sophia Fang <sophiaqfang@gmail.com>
2018-06-27 23:25:44 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -20,7 +20,7 @@ function pressIt(){
 }
 
 function submitIt(){
-  $().on('submit', function(){
+  $('form').on('submit', function(){
     alert("Your form is going to be submitted now.");
   });
 }
